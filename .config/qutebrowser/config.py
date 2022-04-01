2d413f6e0d599cc079abe59a6f0409f5aa3a06f9
Arsalan Kazmi <sonicspeed848@gmail.com>
2022-04-01 22:59:56 +0100

    Mostly font stuff

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -15,17 +15,17 @@ c.bindings.commands['normal'] = {
 }
 c.editor.command = ['gvim', '{file}']
 config.set("colors.webpage.preferred_color_scheme", "dark")
-config.set("fonts.default_family", "Cascadia Code")
+config.set("fonts.default_family", "Iosevka Extended")
 config.set("url.start_pages", "https://that1m8head.github.io")
-config.set("fonts.web.family.standard", "Sitka Display")
-config.set("fonts.web.family.fixed", "Cascadia Code PL")
-config.set("fonts.web.family.serif", "Sitka Display")
-config.set("fonts.web.family.sans_serif", "Segoe UI")
+config.set("fonts.web.family.standard", "Liberation Serif")
+config.set("fonts.web.family.fixed", "Iosevka Extended")
+config.set("fonts.web.family.serif", "Liberation Serif")
+config.set("fonts.web.family.sans_serif", "Liberation Sans")
 config.set("tabs.show", "multiple")
-config.set("colors.tabs.odd.fg", "#aaa");
-config.set("colors.tabs.even.fg", "#aaa");
-config.set("colors.tabs.odd.bg", "black");
-config.set("colors.tabs.even.bg", "black");
+config.set("colors.tabs.odd.fg", "#aaa")
+config.set("colors.tabs.even.fg", "#aaa")
+config.set("colors.tabs.odd.bg", "black")
+config.set("colors.tabs.even.bg", "black")
 config.set("downloads.remove_finished", 10)
 c.tabs.padding = {"bottom": 5, "left": 10, "right": 10, "top": 5}
 c.statusbar.padding = {"bottom": 5, "left": 10, "right": 10, "top": 5}

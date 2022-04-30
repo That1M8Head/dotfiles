6a7243f67d82436431bd20f895ba00ea41c9f851
Arsalan Kazmi <sonicspeed848@gmail.com>
2022-04-30 18:54:04 +0100

    Update qutebrowser config

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,33 +1,52 @@
+# Don't load autoconfig
 config.load_autoconfig(False)
+
+# Key bindings
 c.bindings.commands['normal'] = {
-    'J': 'tab-prev',
-    'K': 'tab-next',
     '<alt-x>': 'set-cmd-text :',
+    '<space>ff': 'set-cmd-text -s :open',
     '<space>fp': 'config-edit',
     '<space>rc': 'config-source',
     '<space>h': 'back',
-    '<space>j': 'tab-prev',
-    '<space>k': 'tab-next',
+    '<space>k': 'tab-prev',
+    '<space>j': 'tab-next',
+    '<space>K': 'tab-move -',
+    '<space>J': 'tab-move +',
+    '<ctrl-k>': 'tab-move -',
+    '<ctrl-j>': 'tab-move +',
     '<space>l': 'forward',
     '<space>t': 'open -t',
     '<space>qq': 'quit',
     '<space>qr': 'restart',
+    '<space>bk': 'tab-close',
     '<space>yw': 'spawn -d mpv {url}',
     '<space>ya': 'spawn -d mpv -x {url}',
 }
-c.editor.command = ['gvim', '{file}']
+
+# Fonts
+config.set("fonts.default_family", "JetBrains Mono")
+config.set("fonts.web.family.standard", "Martel")
+config.set("fonts.web.family.fixed", "JetBrains Mono")
+config.set("fonts.web.family.serif", "Martel")
+config.set("fonts.web.family.sans_serif", "Segoe UI")
+
+# Colours
+bg = "#010122";
+fg = "#AAAAAA";
 config.set("colors.webpage.preferred_color_scheme", "dark")
-config.set("fonts.default_family", "Iosevka Extended")
+config.set("colors.tabs.odd.fg", fg)
+config.set("colors.tabs.even.fg", fg)
+config.set("colors.tabs.odd.bg", bg)
+config.set("colors.tabs.even.bg", bg)
+config.set("colors.tabs.bar.bg", bg)
+config.set("colors.tabs.selected.odd.bg", bg)
+config.set("colors.tabs.selected.even.bg", bg)
+config.set("colors.statusbar.normal.bg", bg)
+
+# Other options
 config.set("url.start_pages", "https://that1m8head.github.io")
-config.set("fonts.web.family.standard", "Liberation Serif")
-config.set("fonts.web.family.fixed", "Iosevka Extended")
-config.set("fonts.web.family.serif", "Liberation Serif")
-config.set("fonts.web.family.sans_serif", "Liberation Sans")
 config.set("tabs.show", "multiple")
-config.set("colors.tabs.odd.fg", "#aaa")
-config.set("colors.tabs.even.fg", "#aaa")
-config.set("colors.tabs.odd.bg", "black")
-config.set("colors.tabs.even.bg", "black")
 config.set("downloads.remove_finished", 10)
+c.editor.command = ['emacs', '{file}']
 c.tabs.padding = {"bottom": 5, "left": 10, "right": 10, "top": 5}
 c.statusbar.padding = {"bottom": 5, "left": 10, "right": 10, "top": 5}
